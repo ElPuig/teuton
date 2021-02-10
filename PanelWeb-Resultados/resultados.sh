34e292897fd8ce2e8ff4a612ab18134352e8bda9
JuanMoroteP <45626958+JuanMoroteP@users.noreply.github.com>
2021-02-11 00:44:52 +0100

    Add files via upload

--- PanelWeb-Resultados/resultados.sh
+++ PanelWeb-Resultados/resultados.sh
@@ -2,15 +2,15 @@
 if [[ -d "${1}" && -n "${2}" ]] 
 then
 	echo "generando resultados...."
-	echo "Comprobacion" > /tmp/resultados.csv
-	grep 'Description' $1case-01.txt | cut -d":" -f2 | cut -c2- >> /tmp/resultados.csv
+	echo "Comprobacion" > $1resultados.csv
+	grep 'Description' $1case-01.txt | cut -d":" -f2 | cut -c2- >> $1resultados.csv
 	
 	for i in $(ls $1case-*.txt)
 	do
-		    echo $i | tr "-" "\n"|tail -n 1  | cut -d"." -f1 > /tmp/resultados01.csv
-		    grep '/1.0)' $i | cut -d"(" -f2|cut -d"/" -f1 >> /tmp/resultados01.csv
-		    paste -d ';' /tmp/resultados.csv /tmp/resultados01.csv > /tmp/resultados_final.csv
-		    cat /tmp/resultados_final.csv > /tmp/resultados.csv
+		    echo $i | tr "-" "\n"|tail -n 1  | cut -d"." -f1 > $1resultados01.csv
+		    grep '/1.0)' $i | cut -d"(" -f2|cut -d"/" -f1 >> $1resultados01.csv
+		    paste -d ';' $1resultados.csv $1resultados01.csv > $1resultados_final.csv
+		    cat $1resultados_final.csv > $1resultados.csv
 	done
 
 	echo "<head><meta http-equiv='refresh' content='30'></head>" > $2
@@ -22,10 +22,10 @@ then
 			CEROS=$(echo "${HT//'<td>0.0</td>'/<td bgcolor=FFCCCD>--</td>}")
 			echo "${CEROS//'<td>1.0</td>'/<td bgcolor=CCFFCC>OK</td>}"  >>$2
 			tit=""
-	done < /tmp/resultados_final.csv 
+	done < $1resultados_final.csv 
 	echo "</table></td></tr><tr><td>" >> $2
-	echo "<table border='1' align='left' font face='monospace'><tbody><tr><td>" >> $2
-	
+	echo "<table border='1' align='left' font face='Courier New'><tbody><tr><td>" >> $2
+	#cp $1resultados.csv $1resultados/resultados$(date +%y_%m_%d.%H_%M_%S).csv
 	date >> $2
 	while read INPUT
 	do
@@ -41,7 +41,10 @@ then
 	echo "<br>
 	</td></tr></tbody></table></td></tr></tabla>" >> $2
 	sudo mv $2 /var/www/html/$2
-	rm $1case-*.txt
+	#cat $1moodle.csv >> $1resultadosmoodle.csv
+	sed '/,0.0,/d' $1moodle.csv | sed  '/MoodleID/d' >> $1resultadosmoodle.csv
+
+	#rm $1case-*.txt
 	echo "................................................."
 	echo "........... Finalizada exportacion .............."
 	echo "................................................."
